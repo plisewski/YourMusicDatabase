dad92e690343fe32bb3d1797d1370a0894cb3206
Piotr Lisewski <p.lisewski@hotmail.com>
2018-02-22 23:13:38 +0100

    Add project files.

---
+++ YourMusicDatabase/Model/MusicAlbumsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourMusicDatabase.Model
+{
+    public class MusicAlbumsModel
+    {
+        public int AlbumID { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public Genre AlbumGenre { get; set; }
+
+    }
+    public enum Genre
+    {
+        Other,
+        Blues,
+	    Country,            
+	    Electronic,
+	    Folk,
+	    HipHop,
+	    Jazz,	    
+	    Pop,
+	    Soul,
+	    Rock
+    }
+}
