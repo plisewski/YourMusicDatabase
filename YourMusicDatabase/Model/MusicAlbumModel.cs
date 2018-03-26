ac951188bd892ab9ecc68f0a7b6268d55a34ae1a
Piotr Lisewski <p.lisewski@hotmail.com>
2018-03-27 00:35:01 +0200

    Redesigned both Model and ViewModel. Finished implementation of core app. functionality - CRUD operations.

---
+++ YourMusicDatabase/Model/MusicAlbumModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace YourMusicDatabase.Model
+{
+    public enum Genre { Blues, Country, Electronic, Folk, HipHop, Jazz, Pop, Rock, Soul, Other }
+
+    [SerializableAttribute]
+    public class MusicAlbumModel
+    {
+        #region Properties
+
+        public string Artist { get; set; }
+        public string AlbumTitle { get; set; }
+        public Genre AlbumGenre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime AddedDate { get; set; }
+
+        #endregion
+
+        #region Methods
+        public static Genre ParseGenre(string descriptionGenre)
+        {
+            switch (descriptionGenre)
+            {
+                case "Blues":
+                    return Genre.Blues;
+                case "Country":
+                    return Genre.Country;
+                case "Electronic":
+                    return Genre.Electronic;
+                case "Folk":
+                    return Genre.Folk;
+                case "HipHop":
+                    return Genre.HipHop;
+                case "Jazz":
+                    return Genre.Jazz;
+                case "Pop":
+                    return Genre.Pop;
+                case "Rock":
+                    return Genre.Rock;
+                case "Soul":
+                    return Genre.Soul;
+                case "Other":
+                    return Genre.Other;
+                default:
+                    throw new Exception("Unrecognized Genre Type");
+            }
+        }
+
+        // https://stackoverflow.com/questions/129389/how-do-you-do-a-deep-copy-of-an-object-in-net-c-specifically/129395#129395
+        public static T DeepClone<T>(T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ YourMusicDatabase/ViewModel/MusicAlbumViewModel.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace YourMusicDatabase.ViewModel
+{
+    #region usings
+
+    using Model;
+    using System.Windows;
+    using System.Windows.Input;
+
+    #endregion
+
+    public class MusicAlbumViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private MusicAlbumModel albumModel = new MusicAlbumModel();        
+        private const string filePath = "../../MusicAlbumsDB.xml";
+        private List<MusicAlbumModel> musicAlbumsList = new List<MusicAlbumModel>();
+        private List<MusicAlbumModel> musicAlbumsListUpdated;
+        private int? _selectedAlbumIndex;
+
+        #endregion
+
+        #region Properties
+
+        public string Artist
+        {
+            get { return albumModel.Artist; }
+            set
+            {
+                albumModel.Artist = value;
+                OnPropertyChanged(nameof(Artist));
+            }
+        }
+
+        public string AlbumTitle
+        {
+            get { return albumModel.AlbumTitle; }
+            set
+            {
+                albumModel.AlbumTitle = value;
+                OnPropertyChanged(nameof(AlbumTitle));
+            }
+        }
+
+        public Genre AlbumGenre
+        {
+            get { return albumModel.AlbumGenre; }
+            set
+            {
+                albumModel.AlbumGenre = value;
+                OnPropertyChanged(nameof(AlbumGenre));
+            }
+        }
+
+        public IEnumerable<Genre> GenreValues
+        {
+            get { return Enum.GetValues(typeof(Genre)).Cast<Genre>(); }
+        }
+
+        public DateTime ReleaseDate
+        {
+            get { return albumModel.ReleaseDate; }
+            set
+            {
+                albumModel.ReleaseDate = value;
+                OnPropertyChanged(nameof(ReleaseDate));
+            }
+        }
+
+        public DateTime AddedDate
+        {
+            get { return albumModel.AddedDate; }
+            set
+            {
+                albumModel.AddedDate = value;
+                OnPropertyChanged(nameof(ReleaseDate));
+            }
+        }
+
+        public List<MusicAlbumModel> MusicAlbumsList
+        {
+            get { return musicAlbumsList; }            
+            set
+            {
+                musicAlbumsList = value;
+                OnPropertyChanged(nameof(MusicAlbumsList));
+            }
+        }
+
+        public List<MusicAlbumModel> MusicAlbumsListUpdated
+        {
+            get { return musicAlbumsListUpdated; }
+            set
+            {
+                musicAlbumsListUpdated = value;
+                OnPropertyChanged(nameof(MusicAlbumsListUpdated));
+            }
+        }
+        
+        public int? SelectedAlbumIndex
+        {
+            get { return _selectedAlbumIndex; }
+            set
+            {
+                _selectedAlbumIndex = value;
+                OnPropertyChanged(nameof(SelectedAlbumIndex));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _create;
+        public ICommand Create
+        {
+            get
+            {
+                if (_create == null)
+                    _create = new RelayCommand(
+                        p =>
+                        {                            
+                            AddedDate = DateTime.Now;                            
+                            XmlService.Create(filePath, albumModel);
+                            MessageBox.Show("Your album has been added!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                            Artist = "";
+                            AlbumTitle = "";
+                            AlbumGenre = Genre.Blues;                         
+                        },
+                        p =>
+                        {
+                            return !string.IsNullOrEmpty(Artist) && !string.IsNullOrEmpty(AlbumTitle) && ReleaseDate < DateTime.Now && ReleaseDate.Year > 1;
+                        }
+                );
+                return _create;
+            }
+        }
+
+        private ICommand _read;
+        public ICommand Read
+        {
+            get
+            {
+                if (_read == null)
+                    _read = new RelayCommand(
+                        p =>
+                        {
+                            MusicAlbumsList = XmlService.Read(filePath);
+                            MusicAlbumsListUpdated = MusicAlbumModel.DeepClone(musicAlbumsList);
+                        }
+                );
+                return _read;
+            }
+        }
+
+        private ICommand _update;
+        public ICommand Update
+        {
+            get
+            {                
+                if (_update == null)
+                    _update = new RelayCommand(
+                        p =>
+                        {
+                            int selectedAlbumIndex = (int)_selectedAlbumIndex;
+
+                            string artistToUpdate = musicAlbumsList[selectedAlbumIndex].Artist;
+                            string albumTitleToUpdate = musicAlbumsList[selectedAlbumIndex].AlbumTitle;
+                            string newArtist = musicAlbumsListUpdated[selectedAlbumIndex].Artist;
+                            string newAlbumTitle = musicAlbumsListUpdated[selectedAlbumIndex].AlbumTitle;
+                            Genre newAlbumGenre = musicAlbumsListUpdated[selectedAlbumIndex].AlbumGenre;
+                            DateTime newReleaseDate = musicAlbumsListUpdated[selectedAlbumIndex].ReleaseDate;                            
+
+                            if (newReleaseDate > DateTime.Now)
+                            {
+                                MessageBox.Show("New release date cannot be in future", "Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                                return;
+                            }                                
+
+                            XmlService.Update(filePath, artistToUpdate, albumTitleToUpdate, newArtist, newAlbumTitle, newAlbumGenre, newReleaseDate);
+                            MessageBox.Show("Your album database has been updated!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                            MusicAlbumsList = MusicAlbumModel.DeepClone(musicAlbumsListUpdated);
+                        },
+                        p =>
+                        {
+                            int selectedAlbumIndex;
+
+                            if (!_selectedAlbumIndex.HasValue)
+                                return false;
+                            else
+                                selectedAlbumIndex = (int)_selectedAlbumIndex;
+
+                            return _selectedAlbumIndex != null && (musicAlbumsList[selectedAlbumIndex].Artist != musicAlbumsListUpdated[selectedAlbumIndex].Artist 
+                                || musicAlbumsList[selectedAlbumIndex].AlbumTitle != musicAlbumsListUpdated[selectedAlbumIndex].AlbumTitle
+                                || musicAlbumsList[selectedAlbumIndex].AlbumGenre != musicAlbumsListUpdated[selectedAlbumIndex].AlbumGenre
+                                || musicAlbumsList[selectedAlbumIndex].ReleaseDate != musicAlbumsListUpdated[selectedAlbumIndex].ReleaseDate);
+                        }
+                );
+                return _update;
+            }
+        }
+
+        private ICommand _delete;
+        public ICommand Delete
+        {
+            get
+            {
+                if (_delete == null)
+                    _delete = new RelayCommand(
+                        p =>
+                        {
+                            int selectedAlbumIndex = (int)_selectedAlbumIndex;
+
+                            string artistToDelete = musicAlbumsList[selectedAlbumIndex].Artist;
+                            string albumTitleToDelete = musicAlbumsList[selectedAlbumIndex].AlbumTitle;
+
+                            XmlService.Delete(filePath, artistToDelete, albumTitleToDelete);
+                            MessageBox.Show("Your album has been deleted!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                            MusicAlbumsList = XmlService.Read(filePath);
+                            MusicAlbumsListUpdated = MusicAlbumModel.DeepClone(musicAlbumsList);                            
+                        },
+                        p =>
+                        {
+                            return _selectedAlbumIndex.HasValue;
+                        }
+                );
+                return _delete;
+            }
+        }
+
+        #endregion
+    }
+}
